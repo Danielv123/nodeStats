a31581b6911b766af43cc5fb042beaadbf9e73e1
Danielv123 <tranen.bananen@gmail.com>
2016-04-29 16:13:52 -0700

    initial commit

---
+++ jsonapi.js
@@ -0,0 +1,24 @@
+var http = require('http');
+const PORT = process.argv[2]; 
+
+function handleRequest(request, response){
+	x = require('url').parse(request.url, true)
+	console.log(x)
+	if (x.pathname == '/api/unixtime') {
+		date = new Date(x.query.iso);
+		resp = {};
+		resp.unixtime = date.getTime()
+		response.end(JSON.stringify(resp));
+	} else if (x.pathname == '/api/parsetime') {
+		date = new Date(x.query.iso);
+		resp = {};
+		resp.hour = date.getHours();
+		resp.minute = date.getMinutes();
+		resp.second = date.getSeconds();
+		response.end(JSON.stringify(resp));
+	}
+}
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
