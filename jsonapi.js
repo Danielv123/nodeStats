b40f0afff5c723ebe778bc032a7833bfb7dc0872
Danielv123 <tranen.bananen@gmail.com>
2016-04-29 17:56:54 -0700

    Hopefully fixed a code error

--- jsonapi.js
+++ jsonapi.js
@@ -2,13 +2,12 @@ var http = require('http');
 
 function handleRequest(request, response){
 	x = require('url').parse(request.url, true)
-	console.log(x)
+	requestLog();
 	if (x.pathname == '/api/unixtime') {
 		date = new Date(x.query.iso);
 		resp = {};
 		resp.unixtime = date.getTime()
 		response.end(JSON.stringify(resp));
-		requestLog();
 	} else if (x.pathname == '/api/parsetime') {
 		date = new Date(x.query.iso);
 		resp = {};
@@ -16,11 +15,9 @@ function handleRequest(request, response){
 		resp.minute = date.getMinutes();
 		resp.second = date.getSeconds();
 		response.end(JSON.stringify(resp));
-		requestLog();
 	} else if (x.pathname == '/health') {
 		response.writeHead(200);
 		response.end('Server online');
-		requestLog();
 	}
 }
 
@@ -29,6 +26,6 @@ function requestLog(custom) {
 }
 
 var server = http.createServer(handleRequest);
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function(){
+server.listen(3000, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
